d58d1ba352b75c3ef51fc5e053ccfb65445a58d5
TechS2105 <techsovangithub0521@gmail.com>
2025-07-05 03:07:36 +0530

    add two new js file in backend folder(Women.js and Womencollections.js)

---
+++ backend/Womencollections.js
@@ -0,0 +1,45 @@
+const Womencollections = [
+
+    {
+
+        id: 1,
+        image: 'https://images.unsplash.com/photo-1612336307429-8a898d10e223?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Culotte dress',
+        price: '5000',
+        category: "Women's Outfit"
+
+    },
+
+    {
+
+        id: 2,
+        image: 'https://images.unsplash.com/photo-1733043014211-8d699f6a82b1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Wrap around dress',
+        price: '2000',
+        category: "Women's Outfit"
+
+    },
+
+    {
+
+        id: 3,
+        image: 'https://images.unsplash.com/photo-1528813569347-f16dd076e347?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Sundress',
+        price: '3000',
+        category: "Women's Outfit"
+
+    },
+
+    {
+
+        id: 4,
+        image: 'https://plus.unsplash.com/premium_photo-1661425743889-8e9f763afe39?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: " Shift dress",
+        price: '5000',
+        category: "Women's Outfit"
+
+    }
+
+]
+
+export default Womencollections;
